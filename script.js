8d2851995e0273714f7e03e247d9056909c233ea
Namanpam22 <namanpam22@gmail.com>
2022-01-16 23:30:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+
+const conatiner = document.querySelector(".container");
+
+open.addEventListener("click", () => {
+  conatiner.classList.add("show-nav");
+});
+
+close.addEventListener("click", () => {
+  conatiner.classList.remove("show-nav");
+});
